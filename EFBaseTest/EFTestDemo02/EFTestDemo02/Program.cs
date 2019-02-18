16eff49b3debcc5f89adf952f6253d9f349b9c9d
wangqilong <664660754@qq.com>
2019-02-18 23:03:49 +0800

    EF Test Demo2 update

--- EFBaseTest/EFTestDemo02/EFTestDemo02/Program.cs
+++ EFBaseTest/EFTestDemo02/EFTestDemo02/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace EFTestDemo02
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
