09e052372112b5d98282d06992b42827b7ea8c89
wangqilong <664660754@qq.com>
2019-02-19 16:12:06 +0800

    update IRepository

--- EFBaseTest/EFTestDemo03/EFTestDemo03/Data/IRepository.cs
+++ EFBaseTest/EFTestDemo03/EFTestDemo03/Data/IRepository.cs
@@ -8,5 +8,60 @@ namespace EFTestDemo03.Data
 {
     public partial interface IRepository<T> where T : BaseEntity
     {
+        /// <summary>
+        /// Get Entity by id
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        T GetById(object id);
+
+        /// <summary>
+        /// insert entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Insert(T entity);
+
+        /// <summary>
+        /// Insert Entity with Return Value
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        int InsertHaveReturnVal(T entity);
+
+        /// <summary>
+        /// Insert entities
+        /// </summary>
+        /// <param name="entities">entities</param>
+        /// <returns></returns>
+        void Insert(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Update Entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Update entities
+        /// </summary>
+        /// <param name="entities">entities</param>
+        void Update(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(T entity);
+
+        /// <summary>
+        /// Delete entities
+        /// </summary>
+        /// <param name="entities"></param>
+        void Delete(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Get a table
+        /// </summary>
+        IQueryable<T> Table { get; }
     }
 }
