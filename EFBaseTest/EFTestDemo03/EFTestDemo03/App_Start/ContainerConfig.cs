97a33db194e26c04cc6608d715632cd00c7e1fb8
wangqilong <664660754@qq.com>
2019-02-21 09:51:12 +0800

    EFTestDemo 03 Update

---
+++ EFBaseTest/EFTestDemo03/EFTestDemo03/App_Start/ContainerConfig.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTestDemo03.App_Start
+{
+    public class ContainerConfig
+    {
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ EFBaseTest/EFTestDemo03/EFTestDemo03/Data/ObjectContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTestDemo03.Data
+{
+    public class ObjectContext : DbContext, IDbContext
+    {
+        public bool ProxyCreationEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool AutoDetectChangesEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Detach(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteSqlCommand(string sql, bool doNotEnsureTransaction = false, int? timeout = null, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TEntity> ExecuteStoredProcedureList<TEntity>(string commandText, params object[] parameters) where TEntity : BaseEntity, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TElement> SqlQuery<TElement>(string sql, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        IDbSet<TEntity> IDbContext.Set<TEntity>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
