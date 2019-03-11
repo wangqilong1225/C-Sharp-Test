f904dacca85ceccbeb5f0bb5026f4799a64fe00f
wangqilong <664660754@qq.com>
2019-03-11 22:56:08 +0800

    add ExecuteScalar function

--- SQLDemo/SQLTestDemo1/SQLTestDemo1/Program.cs
+++ SQLDemo/SQLTestDemo1/SQLTestDemo1/Program.cs
@@ -9,15 +9,25 @@ using SQLTestDemo1.Model;
 
 namespace SQLTestDemo1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            List<Student> StuList = new List<Student>();
+
+            //ExecuteReader()方法
+            FunExecuteReader();
+            // ExecuteScalar()方法
+            FunExecuteScalar();
+
+        }
+
+        /// <summary>
+        /// ExecuteScalar()方法
+        /// </summary>
+        public static void FunExecuteScalar()
+        {
             using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString))
-            //string connString = "Server=.;Database=test;UID=sa;PWD=123456;";
-            //SqlConnection conn = new SqlConnection();
-            //conn.ConnectionString = connString;
+
             using (SqlCommand cmd = new SqlCommand())
             {
                 cmd.CommandType = CommandType.Text; //默认 Text
@@ -25,7 +35,39 @@ namespace SQLTestDemo1
                 cmd.CommandText = "SELECT * FROM Student";
                 cmd.Connection = conn;
                 conn.Open();
+
+                //查询返回的结果集中的第一行的第一列
+                int id = (Int32)cmd.ExecuteScalar();
                 
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// ExecuteNonQuery()方法
+        /// </summary>
+        public static void FunExecuteNonQuery()
+        {
+            
+                
+        }
+        
+        /// <summary>
+        ///  ExecuteReader()方法
+        /// </summary>
+        public static void FunExecuteReader()
+        {
+            List<Student> StuList = new List<Student>();
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString))
+
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.CommandType = CommandType.Text; //默认 Text
+                cmd.CommandTimeout = 30;
+                cmd.CommandText = "SELECT * FROM Student";
+                cmd.Connection = conn;
+                conn.Open();
+
                 SqlDataReader reader = cmd.ExecuteReader();
 
                 while (reader.Read())
@@ -37,9 +79,10 @@ namespace SQLTestDemo1
                     stu.score = Convert.ToInt32(reader["score"]);
                     StuList.Add(stu);
                 }
-                if (StuList!=null)
-                {              
-                    foreach (Student stu in StuList) {
+                if (StuList != null)
+                {
+                    foreach (Student stu in StuList)
+                    {
                         Console.WriteLine(stu.ToString());
                     }
                 }
