662c7371e393f67f38a3bf5e337a973c8286aa87
wangqilong1225 <664660754@qq.com>
2019-03-17 23:04:58 +0800

    C# Preprocessor instructions

---
+++ BaseDemo/BaseDemo2/BaseDemo2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDemo2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #if DEBUG
+                Console.WriteLine("debug");
+            #endif
+
+            #warning abc
+            Console.WriteLine("warning");
+           
+            Console.ReadKey();
+        }
+    }
+}
