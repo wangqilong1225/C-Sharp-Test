489e8d13f2015fe40a32320155c6991d7a792c98
wangqilong1225 <664660754@qq.com>
2019-04-03 23:43:24 +0800

    dataTable sort about date

---
+++ JSDemo/JSDataTable/JSDataTable/Models/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSDataTable.Models
+{
+    public class People
+    {
+        public int ID;
+        public string Name;
+        public string Address;
+        public string Description;
+        public DateTime BirthDay;
+        public DateTime UpdatedDate;
+    }
+}
