b8218c9052929859f882c477a06bfcbfaa72bfc2
wangqilong1225 <664660754@qq.com>
2019-05-25 12:02:40 +0800

    LinqAndLamdaDemo

---
+++ LinqAndLamdaDemo/LinqAndLamdaDemo/Model/People.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLamdaDemo.Model
+{
+    public class People
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public string ToString()
+        {
+            return "ID:"+ID+" ,Name:"+Name+" ,Age:"+Age+" ,"+Age+" ,Address:"+Address;
+        }
+    }
+}
