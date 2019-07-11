9ae722baaddf8d557092e1eac75af933eacc2e9b
wangqilong1225 <664660754@qq.com>
2019-07-11 17:47:38 +0800

    route debug

---
+++ RouteDedug/RouteDedug/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RouteDedug.MvcApplication" Language="C#" %>
---
+++ RouteDedug/RouteDedug/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using RouteDebug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RouteDedug
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //RegisterRoutes(RouteTable.Routes);
+            //PreApplicationStart.Start();
+            //RouteDebug.RouteDebugger.RewriteRoutesForTesting(RouteTable.Routes);
+        }
+
+    }
+}
