71fdcd819cef26030f891beec27c6a8804cd571a
wangqilong1225 <664660754@qq.com>
2019-07-25 21:39:24 +0800

    单元测试

---
+++ FilterDemo/FilterDemoTests/Models/MyExceptionFilterAttributeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilterDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDemo.Models.Tests
+{
+    [TestClass()]
+    public class MyExceptionFilterAttributeTests
+    {
+        [TestMethod()]
+        public void OnExceptionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
