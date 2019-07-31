c4d899aded77c6c8d871389336c0bdfde05a04b1
王旗龙 <wangqilong@basechina.net>
2019-07-31 16:55:55 +0800

    autofac demo

---
+++ AutoFacDemo/AutoFacDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AutoFacDemo.Service;
+using System.Web.Mvc;
+
+namespace AutoFacDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICalculate _calculate;
+
+        public HomeController()
+        {
+            _calculate = DependencyResolver.Current.GetService<ICalculate>();
+        }
+       
+        public ActionResult Index()
+        {
+            int c=_calculate.add(1,2);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AutoFacDemo/AutoFacDemo/Service/ICalculate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoFacDemo.Service
+{
+    public interface ICalculate
+    {
+        int add(int a,int b);
+    }
+}
