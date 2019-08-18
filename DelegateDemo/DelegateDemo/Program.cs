579c2febbc15bacda2238b7faa51417b1d53cbdf
wangqilong1225 <664660754@qq.com>
2019-08-18 22:28:53 +0800

    delegate demo

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    public class Program
+    {
+        public delegate int Method(int num);
+        public static void Main(string[] args)
+        {
+            Method m = Add1;
+            m += Add2;
+            Calculate(m,10);
+            Console.ReadKey();
+        }
+
+        public static int Add1(int a)
+        {
+            int b = 10 + a;
+            Console.WriteLine("——Add1———");
+            return b;
+
+        }
+
+        public static int Add2(int a)
+        {
+            int b = 10 - a;
+            Console.WriteLine("——Add2———");
+            return b;
+
+        }
+
+        public static void Calculate(Method ex, int a)
+        {
+            var result = ex(a);
+            Console.WriteLine(result + "\n");
+        }
+    }
+}
