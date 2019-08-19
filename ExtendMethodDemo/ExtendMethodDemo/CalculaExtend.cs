105b91167489e18fc6a2151ef4c4cdc5030c421a
wangqilong1225 <664660754@qq.com>
2019-08-19 22:34:39 +0800

    Extend Method Demo

---
+++ ExtendMethodDemo/ExtendMethodDemo/CalculaExtend.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendMethodDemo
+{
+    // 1.必须是定义在静态类中的静态方法；
+    // 2.第一个参数的类型是要扩展的类型；
+    // 3.第一个参数需要添加this关键字以标识其为扩展方法。
+    
+    public static class MyString
+    {
+        public static string ReturnString(this string s)
+        {
+            return "string类的扩展方法ReturnString";
+        } 
+
+        public static string AddBlogName(this Blog blog,string name)
+        {
+            string ResName =blog.RenName(name)+"的博客";
+            return ResName;
+        }
+
+        public static List<int> MyWhere(this List<int> list,Func<int,bool> func)
+        {
+            List<int> newList = new List<int>();
+            foreach (int i in list)
+            {
+                if (func(i))
+                {
+                    newList.Add(i);
+                }
+            }
+            return newList;
+        }
+
+    }
+}
---
+++ ExtendMethodDemo/ExtendMethodDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendMethodDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = "";
+            string result = str.ReturnString();
+
+            Blog blog = new Blog();
+            string blogName = blog.AddBlogName("小龙人");
+
+            List<int> list = new List<int> { 3, 7, 2, 1, 8, 15, 99 };
+            List<int> newList = list.MyWhere(a => a > 5);
+        }
+    }
+}
