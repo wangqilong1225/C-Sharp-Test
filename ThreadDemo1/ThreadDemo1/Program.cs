bbbc45a7b57cb9dc6932a5e924fae325afa0a82e
wangqilong1225 <664660754@qq.com>
2019-08-25 23:34:30 +0800

    Thread Demo

---
+++ ThreadDemo1/ThreadDemo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(()=> {
+
+            }));
+
+            //Start Suspend  resume  intterupt  about
+        }
+    }
+}
